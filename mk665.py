6ed0f96c9128398c74439d3728b6e754091e9f72
Armanx007 <102764670+Armanx007@users.noreply.github.com>
2022-05-18 20:03:33 +0500

    Lol.py

---
+++ mk665.py
@@ -0,0 +1,18 @@
+import os, platform,time
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+from time import sleep
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from mk665 import _site_view_
+    print("\n Congratulations! Your device supported!\n")
+    time.sleep(3)
+    _site_view_()
+elif bit == '32bit':
+    from mk665 import _site_view_
+    print("\n Congratulations! Your device supported!\n")
+    time.sleep(3)
+    _site_view_()
